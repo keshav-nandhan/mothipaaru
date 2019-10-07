d4064016b522fea1e72c5b4f60d7b6ab21669364
Keshav Nandhan <keshavnandhan94@gmail.com>
2019-10-07 17:42:04 +0530

    firebase integration

--- mothipaaru_clientapp/src/master/titlebar.jsx
+++ mothipaaru_clientapp/src/master/titlebar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 
  class Titlebar extends Component {
     render() {
@@ -10,7 +11,9 @@ import Col from 'react-bootstrap/Col'
             <Container>
             <Row>
               <Col md={6}><p>Dummy Titile</p></Col>
-              <Col md={{ span: 4, offset: 2 }}>Register</Col>
+              <Col md={{ span: 4, offset: 2 }}>
+              <Button variant="light">Register</Button>
+              </Col>
             </Row>
           </Container>
           </div>
