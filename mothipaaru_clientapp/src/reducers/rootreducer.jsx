f0c5662ab2ca917424e22193b2b4424a5608e8c8
keshav-nandhan <37911262+keshav-nandhan@users.noreply.github.com>
2019-10-14 21:17:26 +0530

    Oct 14
    Oct 14

--- mothipaaru_clientapp/src/reducers/rootreducer.jsx
+++ mothipaaru_clientapp/src/reducers/rootreducer.jsx
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
 import registerUser_Reducer  from './userreducer'
+import { createStore,compose } from 'redux'
+import { ReactReduxFirebaseProvider, firebaseReducer } from 'react-redux-firebase'
+import { createFirestoreInstance, firestoreReducer } from 'redux-firestore'
+
 const rootReducer = combineReducers({
-users: registerUser_Reducer
+users: registerUser_Reducer,
+firebase: firebaseReducer,
+firestore: firestoreReducer
 });
  
 export default rootReducer;
--- mothipaaru_clientapp/src/reducers/userreducer.jsx
+++ mothipaaru_clientapp/src/reducers/userreducer.jsx
@@ -1,17 +1,41 @@
-let users = [
-    {id:1, user:"Keshav"},
-    {id:2, user:"Nandhan"}
-  ]
-  const registerUser_Reducer = (state = users, action) => {
-  const { user } = action
-  switch(action.type){
-    case 'register_User':
-     return [
-      ...state,
-      user
-     ]
-    default:
-     return state  
-   }
+import React from 'react'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+
+function usersList({ firebase, users }) {
+  // Build Todos list if todos exist and are loaded
+  if (!isLoaded(users)) {
+    return <div>Loading...</div>
   }
-  export default registerUser_Reducer
+  if (isEmpty(users)) {
+    return <div>No nearby users</div>
+  }
+  return (JSON.stringify(users, null, 2));
+}
+// let users = [
+//     {id:1, user:"Keshav"},
+//     {id:2, user:"Nandhan"}
+//   ]
+  // const registerUser_Reducer = (state = users, action) => {
+  // const { user } = action
+  // switch(action.type){
+  //   case 'register_User':
+  //    return [
+  //     ...state,
+  //     user
+  //    ]
+  //   default:
+  //    return state  
+  //  }
+  // }
+
+  const registerUser_Reducer = compose(
+    firebaseConnect((props) => [
+      { path: 'users' }
+    ]),connect((state) => ({
+      users: state.firebase.data.users,
+    }))
+  )
+  
+  export default registerUser_Reducer(usersList)
